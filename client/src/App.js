d067e147128133306ef93355c681065cb068664e
Skhull <sdev.skhull@gmail.com>
2023-03-09 00:19:17 +0530

    Change structure of files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 // import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <section className="min-h-screen w-full relative">
+      <Header />
+      <NavBar />
       <Home />
     </section>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,5 @@
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
-
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <NavBar />
-    </>
-  );
+  return <div></div>;
 };
 
 export default Home;
