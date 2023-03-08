170c0a75e87b63d3f280686ca821aa6213344624
Skhull <sdev.skhull@gmail.com>
2023-03-09 00:22:33 +0530

    Add Routes in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <section className="min-h-screen w-full relative">
-      <Header />
-      <NavBar />
-      <Home />
-    </section>
+    <Router>
+      <section className="min-h-screen w-full relative">
+        <Header />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </section>
+    </Router>
   );
 }
 
