075642d3044f9c8545a4142d79116ef1436cac6a
Skhull <sdev.skhull@gmail.com>
2023-03-09 18:30:29 +0530

    navbar 1

--- client/src/components/NavItem.jsx
+++ client/src/components/NavItem.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const NavItem = (props) => {
   return (
     <div className="flex items-center gap-3">
-      <img src={props.type} alt={props.altText} className="w-[50px]" />
-      <p>{props.altText.toUpperCase()}</p>
+      <img
+        src={props.type}
+        alt={props.altText}
+        className="2xl:w-[50px] w-[30px]"
+      />
+      {/* <p>{props.altText.toUpperCase()}</p> */}
     </div>
   );
 };
