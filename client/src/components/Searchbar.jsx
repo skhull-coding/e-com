ec378cda734082caf1dd013c90dc5363cc576628
Skhull <sdev.skhull@gmail.com>
2023-03-09 19:10:52 +0530

    navbar 3

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -14,7 +14,7 @@ const Searchbar = () => {
     >
       <input
         type="text"
-        className="w-full 2xl:text-2xl text-base px-3 py-2 outline-none bg-[#CFEBEF] tracking-wide font-medium rounded-l-lg placeholder:font-thin placeholder:text-black/80"
+        className="w-full 2xl:text-2xl text-base px-5 py-2 outline-none bg-[#CFEBEF] tracking-wide font-medium rounded-l-lg placeholder:font-thin placeholder:text-black/80"
         placeholder="Search Here"
         value={searchValue}
         onChange={(e) => {
