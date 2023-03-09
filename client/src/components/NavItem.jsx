fc3be135cefe4a372542cd7d955c486cda2754b4
Skhull <sdev.skhull@gmail.com>
2023-03-09 19:11:56 +0530

    navbar 4 - text small navitem

--- client/src/components/NavItem.jsx
+++ client/src/components/NavItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavItem = (props) => {
   return (
     <Link className="navItem flex bg-[#172F43] items-center gap-3 hover:brightness-125 p-[25px] relative">
-      <div className="floatingText flex justify-center items-center text-lg tracking-wider overflow-hidden absolute w-0 transition-all text-white h-full left-0 bg-[#172f43]">{props.altText.toUpperCase()}</div>
+      <div className="floatingText flex justify-center items-center 2xl:text-lg text-sm tracking-wider overflow-hidden absolute w-0 transition-all text-white h-full left-0 bg-[#172f43]">{props.altText.toUpperCase()}</div>
       <img
         src={props.type}
         alt={props.altText}
